3a573f437929802e64c57aaf2f0303302fb5543f
Disha Harsoda <110022936+Disha2251@users.noreply.github.com>
2023-08-29 12:25:09 +0530

    Add files via upload

---
+++ Script_3.php
@@ -0,0 +1,8 @@
+<?php
+
+/*3. Develop a PHP program that assigns the value "42" to a variable named $answer and then echoes
+the value along with the text " is the answer to everything."*/
+
+$a=42;
+echo "$a is answer to everything";
+?>

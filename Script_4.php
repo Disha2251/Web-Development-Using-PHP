a9c62de8fea885ad8fc5e87fd12ad27a03a5f067
Disha Harsoda <110022936+Disha2251@users.noreply.github.com>
2023-08-29 12:25:23 +0530

    Add files via upload

---
+++ Script_4.php
@@ -0,0 +1,9 @@
+<?php
+/*4. Write a PHP program that assigns your first name to the variable $firstName and your last name to
+the variable $lastName. Use concatenation to display a greeting message like "Hello, John Doe!".*/
+
+$firstName="Disha ";
+$lastName="Harsoda";
+$fn=$firstName.$lastName;
+echo "Hello,$fn!";
+?>

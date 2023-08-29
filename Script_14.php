bc4391dd6b3202dd31d16df28d7bf596a3af53d1
Disha Harsoda <110022936+Disha2251@users.noreply.github.com>
2023-08-29 13:33:27 +0530

    Add files via upload

---
+++ Script_14.php
@@ -0,0 +1,15 @@
+<?php
+/*14.Create a PHP program that uses echo to display the following text with proper formatting:*/
+
+
+
+    echo "list 1";
+    echo "<br>";
+    echo "&nbsp;&nbsp;&nbsp; list 2";
+    echo "<br>";
+    echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; list 3";
+?>
+   
+
+
+
